e45e2f2f088ca1f984430d3a6c59315e3c58618a
Tim Paine <t.paine154@gmail.com>
2019-11-12 23:28:10 -0500

    start on manual competiion/submission

--- src/ts/widgets/new.ts
+++ src/ts/widgets/new.ts
@@ -27,11 +27,87 @@ class NewSubmissionWidget extends BaseWidget {
 namespace Private {
     export function createNewCompetitionNode(): HTMLDivElement {
         const node = document.createElement("div");
+        node.innerHTML =
+        "<form name=\"newcompetition\" action=\"\">\
+        <h2>New Competition</h2> \
+        <label>Title</label> \
+        <input type=\"text\" placeholder=\"title\" required></input>\
+        \
+        <label>Subtitle</label> \
+        <input type=\"text\" placeholder=\"subtitle\"></input>\
+        \
+        <label>Type</label> \
+        <select name=\"type\">\
+            <option value=\"classify\" selected>Classify</option>\
+            <option value=\"predict\">Predict</option>\
+            <option value=\"cluster\">Cluster</option>\
+        </select>\
+        \
+        <label>Expiration</label> \
+        <input type=\"datetime-local\" required></input>\
+        \
+        <label>Prize</label> \
+        <input type=\"number\" step=\".1\" min=\"0\" placeholder=\"prize\"></input>\
+        \
+        <label>Metric</label> \
+        <select name=\"metric\">\
+            <option value=\"logloss\" selected>Log Loss</option>\
+            <option value=\"absdiff\">Absolute Diff</option>\
+        </select>\
+        \
+        <label>Targets</label> \
+        \
+        <label>Dataset</label> \
+        \
+        <label>Dataset Type</label> \
+        <select name=\"dataset_type\">\
+            <option value=\"json\" selected>JSON</option>\
+            <option value=\"csv\">CSV</option>\
+        </select>\
+        \
+        <label>Dataset Kwargs</label> \
+        \
+        <label>Dataset Key</label> \
+        \
+        <label>Num Classes</label> \
+        <input type=\"number\" step=\"1\" min=\"2\" placeholder=\"number of classes\"></input>\
+        \
+        <label>When</label> \
+        <input type=\"datetime-local\" required></input>\
+        \
+        <label>Answer</label> \
+        \
+        <label>Answer Type</label> \
+        <select name=\"answer_type\">\
+            <option value=\"json\" selected>JSON</option>\
+            <option value=\"csv\">CSV</option>\
+        </select>\
+        \
+        <label>Answer Delay</label> \
+        \
+        <input type=\"submit\" value=\"Create\"></input>\
+        </form>";
         return node;
     }
 
     export function createNewSubmissionNode(): HTMLDivElement {
         const node = document.createElement("div");
+        node.innerHTML =
+        "<form name=\"newsubmission\" action=\"\">\
+        <h2>New Submission</h2> \
+        <label>Competition ID</label> \
+        <input type=\"number\" step=\"1\" min=\"2\" placeholder=\"competition id\"></input>\
+        \
+        <label>Answer</label> \
+        \
+        <label>Answer Type</label> \
+        <select name=\"answer_type\">\
+            <option value=\"json\" selected>JSON</option>\
+            <option value=\"csv\">CSV</option>\
+        </select>\
+        \
+        <input type=\"submit\" value=\"Submit\"></input>\
+        </form>";
         return node;
     }
 }
