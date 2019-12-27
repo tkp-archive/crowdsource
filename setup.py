1931a1616f8a07620c911b0ac66ee1cc59faafa2
Tim Paine <t.paine154@gmail.com>
2019-12-27 15:07:37 -0500

    move requirements to setup

--- setup.py
+++ setup.py
@@ -7,6 +7,35 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+requires = [
+    'cufflinks>=0.17.0',
+    'IPython>=7.4.0',
+    'jinja2>=2.10',
+    'numpy>=1.17.2',
+    'pandas>=0.25.0',
+    'perspective-python>=0.4.0rc6',
+    'psycopg2>=2.7.6.1',
+    'scikit-learn>=0.21.3',
+    'scipy>=1.2.1',
+    'sqlalchemy>=1.3.0',
+    'tornado>=6.0.3',
+    'ujson>=1.35',
+    'validators>=0.12.4',
+]
+
+requires_dev = [
+    'Faker>=1.0.0',
+    'flake8>=3.7.8',
+    'mock',
+    'pybind11>=2.4.0',
+    'pytest>=4.3.0',
+    'pytest-cov>=2.6.1',
+    'pytest-check-links',
+    'pytz>=2018.9',
+    'Sphinx>=1.8.4',
+    'sphinx-markdown-builder>=0.5.2',
+] + requires
+
 setup(
     name='crowdsource',
     version='0.1.0',
@@ -34,11 +63,9 @@ setup(
     zip_safe=False,
     packages=find_packages(exclude=[]),
     include_package_data=True,
+    install_requires=requires,
     extras_require={
-        'dev': ['bumpversion',
-                'pytest-cov >=2.6.1',
-                'codecov',
-                'mock']
+        'dev': requires_dev,
     },
     entry_points={
         'console_scripts': [
