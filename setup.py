58ed22b1c28d3d5fb0fba02a5bec30d9c9d673d9
Tim Paine <t.paine154@gmail.com>
2019-12-28 17:12:49 -0500

    move to bumpversion

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup, find_packages
 from codecs import open
-from os import path
+import io
+import os.path
+pjoin = os.path.join
+here = os.path.abspath(os.path.dirname(__file__))
+name = 'crowdsource'
 
-here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+def get_version(file, name='__version__'):
+    path = os.path.realpath(file)
+    version_ns = {}
+    with io.open(path, encoding="utf8") as f:
+        exec(f.read(), {}, version_ns)
+    return version_ns[name]
+
+
+version = get_version(pjoin(here, name, '_version.py'))
+
+with open(pjoin(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 requires = [
@@ -34,26 +47,21 @@ requires_dev = [
 ] + requires
 
 setup(
-    name='crowdsource',
-    version='0.1.0',
+    name=name,
+    version=version,
     description='Realtime Competitions',
     long_description=long_description,
     url='https://github.com/timkpaine/crowdsource',
-    download_url='https://github.com/timkpaine/crowdsource/archive/v0.1.0.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='Apache 2.0',
 
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     keywords='realtime competition streaming',
